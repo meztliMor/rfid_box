ce528e065887d53468463ffb6b62109a4e2baa07
Michall3 <12702495+Michall3@users.noreply.github.com>
2019-03-19 18:18:23 +0000

    Fixed moving items between queues. Added a fix for converting keys to int when loading from a json file. Added Board __str__.

--- proto2.py
+++ proto2.py
@@ -2,6 +2,7 @@ import copy
 import json
 import nfc
 import nfc.ndef
+import cPickle as pickle
 
 import buttons
 
@@ -34,6 +35,10 @@ class Board(object):
 
         print "Board init done"
 
+    def __str__(self):
+        return "Board(todo_q:{q1}, done_q{q2}, basket_q{q3})".format(q1=json.dumps(self.todo_q), q2=json.dumps(self.done_q), q3=json.dumps(self.basket_q))
+
+
     def start(self):
         self.comms.start()
 
@@ -41,47 +46,82 @@ class Board(object):
         with open(self.dump_path, "w") as f:
             state = {k: getattr(self, k) for k in self.queue_list}
             print state
-            json.dump(state)
+            json.dump(state, f)
+            #json.dump({k: getattr(self, k) for k in self.queue_list}, f)
 
     def load_state(self):
-        with open(self.dump_path, "r") as f:
-            state = json.load(f)
-            print state
-            for k in self.queue_list:
-                setattr(self, k, state[k])
+        def _conv_to_int(o):
+            # modified version of a function found in https://stackoverflow.com/questions/45068797/how-to-convert-string-int-json-into-real-int-with-json-loads/45069099
+    # Note the "unicode" part is only for python2
+            if isinstance(o, str) or isinstance(o, unicode):
+                try:
+                    return int(o)
+                except ValueError:
+                    return o
+            elif isinstance(o, dict):
+                return {_conv_to_int(k): _conv_to_int(v) for k, v in o.items()}
+            #elif isinstance(o, list):
+            #    return [_conv_to_int(v) for v in o]
+            else:
+                return o
+
+        try:
+            with open(self.dump_path, "r") as f:
+                state = json.load(f, object_hook=_conv_to_int)
+                print state
+                for k in self.queue_list:
+                    setattr(self, k, state[k])
+        except (IOError, ValueError) as e:
+            self.clear_queues()
+
+    def clear_queues(self):
+        for k in self.queue_list:
+            setattr(self, k, {})
 
     def find_item(self, tag):
         key = get_tag_id(tag)
         print "find item " + str(key)
         # TODO: check if the task changes?
-        if self.todo_q.get(id):
-            self.mark_done(id)
-        elif self.done_q.get(id):
-            self.mark_todo(id)
-        elif self.basket_q.get(id):
+        if self.todo_q.get(key):
+            print "In TODO"
+            self.mark_done(key)
+        elif self.done_q.get(key):
+            print "IN DONE"
+            self.mark_todo(key)
+        elif self.basket_q.get(key):
             print "In the basket"
-            print self.basket_q[id]
+            print self.basket_q[key]
         elif self.comms.pressed(): # if pressed, add to basket
+            #TODO: pickle and store entire Message or Record object
+            #self.basket_q[id] = copy.deepcopy(tag.ndef.message)
             print "add to the basket"
-            self.basket_q[id] = copy.deepcopy(tag.ndef.message)
+            self.basket_q[key] = copy.deepcopy(tag.ndef.records[0].text)
+            self.save_state()
         else: #ignore
             print "Button not pressed: ignoring"
+        
         return True
 
-    def move_item(self, id, queue_a, queue_b):
-        val = queue_a[id]
-        # use dict.pop instead?
-        del queue_a[id]
-        queue_b[id] = val
-        return val
+    def move_item(self, key, queue_a, queue_b):
+        # TODO: should it check for duplicates? and missing values?
+        #TODO: might need a mutex
+        queue_b[key] = queue_a.pop(key)
+        self.save_state()
+        return queue_b[key]
 
-    def mark_done(self, id):
-        val = move_item(id, self.todo_q, self.done_q)
-        print "\t{d}: {v} - DONE".format(d=id, v=val)
+    def move_basket_to_todo(self):
+        #TODO: might need a mutex
+        self.todo_q.update(self.basket_q)
+        self.basket_q = {}
+        self.save_state()
 
-    def mark_todo(self, id):
-        move_item(id, self.done_q, self.todo_q)
-        print "\t{d}: {v} - TODO".format(d=id, v=val)
+    def mark_done(self, key):
+        val = self.move_item(key, self.todo_q, self.done_q)
+        print "\t{d}: {v} - DONE".format(d=key, v=val)
+
+    def mark_todo(self, key):
+        val = self.move_item(key, self.done_q, self.todo_q)
+        print "\t{d}: {v} - TODO".format(d=key, v=val)
 
 
 def start_reader():
@@ -99,6 +139,7 @@ def test_pn532(tag):
 def run():
     try:
         board = Board()
+        board.load_state()
         clf = start_reader()
         try:
             board.start()
@@ -115,6 +156,7 @@ def run():
         finally:
             #FIXME: make sure it's run on SIGKILL
             clf.close()
+            board.save_state()
     finally:
         #FIXME: repalce this with sys.exceptionhook in buttons
         buttons.cleanup()
